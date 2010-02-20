197424490dd5ef0ab23b76bb25794343aed75f67
Charles Merriam <charles.merriam@gmail.com>
2010-02-20 14:05:37 -0800

    Fixing up setup.py to have dependency and trove categories

--- setup.py
+++ setup.py
@@ -6,9 +6,22 @@ setup(
     author='Charles Merriam',
     author_email='charles.merriam@gmail.com',
     packages=['dectools', 'dectools.test'],
-    url='http://pypi.python.org/pypi/dectools/',
-    license='LICENSE.txt',
-    description='Decorator toolkit library',
+    url='http://charlesmerriam.com/dectools/',
+    license='MIT License',
+    description='decorator toolkit library',
     long_description=open('README.txt').read(),
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        'Natural Language :: English',
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Utilities",
+        ],
+    requires=['decorator (>=3.1)'],
 )
+
 
