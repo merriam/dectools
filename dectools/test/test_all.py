f2dccc65b7aba4351e644f1be945b5f1a62b24fc
Charles Merriam <charles.merriam@gmail.com>
2010-02-27 06:41:35 -0800

    Tests work with nosetests and unittest.  94% coverage.

--- dectools/test/test_all.py
+++ dectools/test/test_all.py
@@ -7,53 +7,54 @@
 # To run these tests, if you have the decorator module in your PYTHONPATH,
 # you can:
 #     1.  $ python test_all.py
-#         This does lots of output, stops on first error.  "All tests done" 
-#         is last line on success.
+#         This does lots of output. 
 #     2.  nosetests
 #         Lots of dots
 #
 # To add tests, follow the boilerplate.  I thought of just doing a
-# quick generate on the fly, but it's better to avoid the metaprogramming.
+# quick generate on the fly, but it's easy to debug a test without 
+# the metaprogramming.
 
-def test_call_before():
-    import test_call_before
+import unittest2 as unittest
 
-def test_call_if():
-    import test_call_if2
+class Test_by_import(unittest.TestCase):
+    def test_call_before(self):
+        import test_call_before
     
-def test_call_if():
-    import test_call_if
-
-def test_call_instead():
-    import test_call_instead
-
-def test_call_once():
-    import test_call_once
-
-def test_make_call_if():
-    import test_make_call_if
-
-def test_make_call_instead():
-    import test_make_call_instead
-
-def test_log():
-    import test_log
-
-def test_make_call_once():
-    import test_make_call_once
-
-def test_dict_as_called():
-    import test_dict_as_called
-
-def test_pre_post():
-    import test_pre_post
-
-def test_invariants():
-    import test_invariants
+    def test_call_if2(self):
+        import test_call_if2
+        
+    def test_call_if(self):
+        import test_call_if
+    
+    def test_call_instead(self):
+        import test_call_instead
+    
+    def test_call_once(self):
+        import test_call_once
+    
+    def test_make_call_if(self):
+        import test_make_call_if
+    
+    def test_make_call_instead(self):
+        import test_make_call_instead
+    
+    def test_log(self):
+        import test_log
+    
+    def test_make_call_once(self):
+        import test_make_call_once
+    
+    def test_dict_as_called(self):
+        import test_dict_as_called
+    
+    def test_pre_post(self):
+        import test_pre_post
+    
+    def test_invariants(self):
+        import test_invariants
 
 if __name__ == "__main__":
-    tests = [func for name, func in locals().items() if name.startswith("test_")]
-    for test in tests:
-        test()
-    print "All tests done..."
+    unittest.main()
+    print("All tests done...")
     
